a166a7c605285edee87c2b3589385d1bf5968187
MrZudro <jmqo2015@gmail.com>
2025-10-20 17:19:38 -0500

    Comentarios de aclaracion

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 //Raiz de Tailwind
 import './index.css'
+
+//Paginas completas con las rutas
 import HomeMq from './pages/HomeMq';
 
 //Aqui se asignan las rutas, al final del desarrollo se agregara la seguridad
