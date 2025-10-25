08f34275a1e0bf65a2a19e2fdb7f1e49c3ef155b
lauraguerrero01 <laura.j.guerrero.r@gmail.com0>
2025-10-24 20:54:09 -0500

    Pagina servicios

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,14 @@ import './index.css'
 //Paginas completas con las rutas
 import HomeMq from './pages/HomeMq';
 import ProductsLg from './pages/ProductsLg';
+import ServicesLg from './pages/ServicesLg';
 
 //Aqui se asignan las rutas, al final del desarrollo se agregara la seguridad
 const rutas = createBrowserRouter([
   { path:'/', element:<HomeMq/>},
-  { path:'/products', element:<ProductsLg/>}
+  { path:'/products', element:<ProductsLg/>},
+  { path:'/services', element:<ServicesLg/>}
+
 ]);
 
 createRoot(document.getElementById('root')).render(
