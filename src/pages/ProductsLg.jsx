ec197bdf95ac545b8513cf62c78160a70cd8a0d9
lauraguerrero01 <laura.j.guerrero.r@gmail.com0>
2025-10-27 17:36:25 -0500

    pagina productos columnas de productos

--- src/pages/ProductsLg.jsx
+++ src/pages/ProductsLg.jsx
@@ -64,7 +64,7 @@ const ProductsLg= () => {
                 ) : (
                     <>
                         {/* GRANDE Y EXTRA GRANDE: Transición fluida de 1 -> 2 -> 3 -> 4 -> 5 -> 6 columnas */}
-                        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6 gap-6">
+                        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6">
                             {products.map(product => (
                                 <ProductCard 
                                     key={product.id} 
